60c7b58dc85e424bcd83948288088e2c5f1a3127
Ernur Torekul <ernurtorekul@gmail.com>
2024-10-18 17:00:40 +0500

    added telega feat77543

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,30 @@
+"use client"
 import "./globals.css";
+import { useEffect } from "react";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    if (typeof window !== "undefined" && !document.getElementById("telegram-web-app-script")) {
+      const script = document.createElement("script");
+      script.src = "https://telegram.org/js/telegram-web-app.js";
+      script.id = "telegram-web-app-script";
+      document.head.appendChild(script);
+      
+      script.onload = () => {
+        console.log("Telegram WebApp script loaded successfully.");
+      };
+    }
+  }, []);
+
   return (
     <html lang="en">
-      <head><script src="https://telegram.org/js/telegram-web-app.js"></script></head>
+      <head>
+        {/* The script is dynamically added to the head */}
+      </head>
       <body>{children}</body>
     </html>
   );
