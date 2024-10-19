dc439e33e9f7a466679ccd8f92ecf8ea1db5b047
zAnz <baidosovich@gmail.com>
2024-10-19 15:54:15 +0500

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,44 +1,42 @@
-"use client";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation"; // Use useRouter from next/navigation
-import Navbar from "../components/Navbar";
-import HrNavbar from "@/app/hr/components/HrNavbar";
-import Switcher from "@/components/custom/switcher";
+"use client"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/navigation" // Use useRouter from next/navigation
+import Navbar from "../components/Navbar"
+import HrNavbar from "@/app/hr/components/HrNavbar"
+import Switcher from "@/components/custom/switcher"
+import "./globals.css"
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-  const [userRole, setUserRole] = useState<string | null>(null);
-  const router = useRouter(); // Use Next.js 13 router for navigation
+  const [userRole, setUserRole] = useState<string | null>(null)
+  const router = useRouter() // Use Next.js 13 router for navigation
 
   useEffect(() => {
-    const savedRole = localStorage.getItem("userRole");
+    const savedRole = localStorage.getItem("userRole")
     if (savedRole) {
-      setUserRole(savedRole);
+      setUserRole(savedRole)
     }
-  }, []);
+  }, [])
 
   const handleRoleSelection = (role: string) => {
-    setUserRole(role);
-    localStorage.setItem("userRole", role);
+    setUserRole(role)
+    localStorage.setItem("userRole", role)
 
     // Use client-side navigation to avoid page reload
     if (role === "employer") {
-      router.push("/hr/home"); // Navigate to HR dashboard
+      router.push("/hr/home") // Navigate to HR dashboard
     } else {
-      router.push("/home"); // Navigate to employee pages
+      router.push("/home") // Navigate to employee pages
     }
-  };
+  }
 
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <script
-          src="https://telegram.org/js/telegram-web-app.js"
-          async
-        ></script>
+        <script src='https://telegram.org/js/telegram-web-app.js' async></script>
       </head>
       <body>
         <div>
@@ -50,5 +48,5 @@ export default function RootLayout({
         {userRole === "employer" ? <HrNavbar /> : <Navbar />}
       </body>
     </html>
-  );
+  )
 }
